01835acb96a3a655289aaef8bd1a86ee0fafb097
Ayush Gupta <ayushgupta197@gmail.com>
2019-10-16 11:46:47 +0530

    Added card-list and card component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { CardList } from "./components/card-list/card-list.component";
+
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users").then(response =>
+      response.json().then(users => this.setState({ monsters: users }))
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
