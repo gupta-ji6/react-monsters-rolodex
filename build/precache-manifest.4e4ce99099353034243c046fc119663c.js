25202c0b646ce61340fd74c19e853af26d981996
gupta-ji6 <ayushgupta197@gmail.com>
2019-10-20 14:14:39 +0530

    added deployment

---
+++ build/precache-manifest.4e4ce99099353034243c046fc119663c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3eabe0676be27171f137b645758037",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "af994a17d37c77c11eb7",
+    "url": "/monsters-rolodex/static/css/main.26e01bf5.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/monsters-rolodex/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "af994a17d37c77c11eb7",
+    "url": "/monsters-rolodex/static/js/main.27b48a1d.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
